76fc4650c92726f053e8a1b463876d2271e51b13
Семён Барышников <lSenichl>
2021-04-18 19:35:08 +0300

    rebuild Lab06

--- lib/Labs/lab06.dart
+++ lib/Labs/lab06.dart
@@ -1,19 +1,41 @@
-import 'package:file_encrypter/file_encrypter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_file_dialog/flutter_file_dialog.dart';
+import 'dart:io';
+import 'package:aes_crypt/aes_crypt.dart';
 
-String key;
+class Lab06 extends StatefulWidget {
+  @override
+  Lab06State createState() => Lab06State();
+}
 
-class Lab06 extends StatelessWidget {
+String encFilepath;
+String decFilepath;
+String srcFilepath;
+
+class Lab06State extends State<Lab06> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: [
-            Text(
-                'Шифрование по методу: "AES-256 in CBC mode with PKCS5 padding"'),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text('Введите пароль для шифрования: ')),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: myController,
+              ),
+            ),
             ElevatedButton.icon(
               onPressed: () async {
                 final params = OpenFileDialogParams();
@@ -21,10 +43,30 @@ class Lab06 extends StatelessWidget {
                     await FlutterFileDialog.pickFile(params: params);
                 print(filePath);
 
-                key = await FileEncrypter.encrypt(
-                  inFilename: filePath,
-                  outFileName: '/storage/emulated/0/Download/encrypted.dat',
-                );
+                srcFilepath = filePath;
+
+                print('Unencrypted source file: $srcFilepath');
+                print('File content: ' +
+                    File(srcFilepath).readAsStringSync() +
+                    '\n');
+
+                var crypt = AesCrypt();
+
+                crypt.setPassword(myController.text);
+                crypt.setOverwriteMode(AesCryptOwMode.warn);
+
+                try {
+                  encFilepath = crypt.encryptFileSync(srcFilepath,
+                      '/storage/emulated/0/Download/testfile_encrypted.aes');
+                  print('The encryption has been completed successfully.');
+                  print('Encrypted file: $encFilepath');
+                } on AesCryptException catch (e) {
+                  if (e.type == AesCryptExceptionType.destFileExists) {
+                    print('The encryption has been completed unsuccessfully.');
+                    print(e.message);
+                  }
+                  return;
+                }
                 print('Encryption Ended');
               },
               icon: Icon(Icons.cloud_download),
@@ -36,11 +78,27 @@ class Lab06 extends StatelessWidget {
                 final filePath =
                     await FlutterFileDialog.pickFile(params: params);
                 print(filePath);
-                await FileEncrypter.decrypt(
-                  key: key,
-                  inFilename: filePath,
-                  outFileName: '/storage/emulated/0/Download/decrypted.txt',
-                );
+
+                var crypt = AesCrypt();
+
+                crypt.setPassword(myController.text);
+                crypt.setOverwriteMode(AesCryptOwMode.warn);
+
+                try {
+                  // Decrypts the file which has been just encrypted and tries to save it under
+                  // another name than source file name.
+                  decFilepath = crypt.decryptFileSync(filePath,
+                      '/storage/emulated/0/Download/testfile_new.txt');
+                  print('The decryption has been completed successfully.');
+                  print('Decrypted file 2: $decFilepath');
+                  print(
+                      'File content: ' + File(decFilepath).readAsStringSync());
+                } on AesCryptException catch (e) {
+                  if (e.type == AesCryptExceptionType.destFileExists) {
+                    print('The decryption has been completed unsuccessfully.');
+                    print(e.message);
+                  }
+                }
                 print('Decryption Ended');
               },
               icon: Icon(Icons.cloud_download),
